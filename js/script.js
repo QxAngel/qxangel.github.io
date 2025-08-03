1175e087ccc76ba8aed47698f19e590ba28167dd
QxAngel <136937878+QxAngel@users.noreply.github.com>
2025-08-03 15:37:30 -0600

    Improve iOS Web App support
    - Ads removed from Web App standalone mode 😞
    - Added detection for standalone Web App mode on iOS.
    - Show a user message informing that the file download has started.

--- js/script.js
+++ js/script.js
@@ -268,15 +268,24 @@ document.addEventListener("DOMContentLoaded", () => {
 
             if (!downloadURL) return;
 
-           alert("When you tap Close, an ad will open in a new tab. This helps keep the site online covering the domain costs. Your download will stay safe and ready here. After the ad opens, you can ignore it, close that tab, and come back here to finish your download. Thanks for helping keep this site online!        — Report issues on Instagram: @6ky_l");
-
-            const adWindow = window.open('https://www.profitableratecpm.com/xuve62am?key=f822534d9d38e28827d14b3fd8af04cb', '_blank');
+            const isStandalone = window.matchMedia('(display-mode: standalone)').matches || window.navigator.standalone === true;
 
             const downloadLink = document.createElement("a");
             downloadLink.href = downloadURL;
             downloadLink.download = "";
             downloadLink.click();
-            
+
+            if (isStandalone) {
+              setTimeout(() => {
+              alert("Web App mode detected. Your file is downloading — this may take a few seconds. Once it's ready, it will appear on your screen automatically. Please close this alert and wait. Do not close or leave the app");
+               }, 300);
+            } else {
+              alert("When you tap Close, an ad will open in a new tab. This helps keep the site online covering the domain costs. Your download will stay safe and ready here. After the ad opens, you can ignore it, close that tab, and come back here to finish your download. Thanks for helping keep this site online!        — Report issues on Instagram: @6ky_l");
+
+              const adWindow = window.open('https://www.profitableratecpm.com/xuve62am?key=f822534d9d38e28827d14b3fd8af04cb', '_blank');
+
+            }
+
           });
 
           const copyDownloadUrlBtn = modalContent.querySelector(".install-copy-url");
@@ -334,3 +343,4 @@ document.addEventListener("DOMContentLoaded", () => {
     });
 
 });
+
